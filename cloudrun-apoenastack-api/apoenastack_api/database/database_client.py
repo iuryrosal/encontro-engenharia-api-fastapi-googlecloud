c2de9ee5fbfdfd750482d2399c1fa8cefb889621
Iury Rosal <iuryrosal@gmail.com>
2024-08-14 15:45:39 -0300

    fix: adjust get_conn

--- cloudrun-apoenastack-api/apoenastack_api/database/database_client.py
+++ cloudrun-apoenastack-api/apoenastack_api/database/database_client.py
@@ -33,25 +33,25 @@ class DatabaseClient:
         else:
             raise Exception(f"Value of ENV variable ({os.environ['ENV']}) invalid")
 
-        def __get_conn(self) -> pg8000.dbapi.Connection:
-            print("Chamada de Sessão da Base de Dados")
-            project_id = os.getenv("PROJECT_ID", "")
-            region = os.getenv("REGION", "southamerica-east1")
-            instance = os.getenv("INSTANCE", "apoena-database")
-            instance_connection_name = f"{project_id}:{region}:{instance}"
-            db_user = os.getenv("DB_USER", "")
-            db_pass = os.getenv("DB_PASS", "")
-            db_name = os.getenv("DB_NAME", "")
+    def __get_conn(self) -> pg8000.dbapi.Connection:
+        print("Chamada de Sessão da Base de Dados")
+        project_id = os.getenv("PROJECT_ID", "")
+        region = os.getenv("REGION", "southamerica-east1")
+        instance = os.getenv("INSTANCE", "apoena-database")
+        instance_connection_name = f"{project_id}:{region}:{instance}"
+        db_user = os.getenv("DB_USER", "")
+        db_pass = os.getenv("DB_PASS", "")
+        db_name = os.getenv("DB_NAME", "")
 
-            ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+        ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
 
-            connector = Connector(ip_type)
+        connector = Connector(ip_type)
 
-            conn = connector.connect(
-                instance_connection_name,
-                "pg8000",
-                user=db_user,
-                password=db_pass,
-                db=db_name
-            )
-            return conn
+        conn = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name
+        )
+        return conn
