09f96ff8c931049a69116e08a556a19b68bd5fb4
Iury Rosal <iuryrosal@gmail.com>
2024-08-14 14:55:04 -0300

    fix: Ajuste do database

--- cloudrun-apoenastack-api/apoenastack_api/app.py
+++ cloudrun-apoenastack-api/apoenastack_api/app.py
@@ -14,22 +14,24 @@ async def health_check():
 
 @app.get("/customers/{cd_customer}", status_code=HTTPStatus.OK, response_model=Customer)
 async def get_customer_by_cd(cd_customer):
+    database = DatabaseClient()
     print(f"GET /customers/{cd_customer}")
-    queried_customer = DatabaseClient() \
-                        .query(Customers) \
-                        .filter_by(cd_customer=cd_customer) \
-                        .first()
+    queried_customer = database() \
+        .query(Customers) \
+        .filter_by(cd_customer=cd_customer) \
+        .first()
     return queried_customer
 
 
 @app.get("/customers/", status_code=HTTPStatus.OK, response_model=list[Customer])
 async def get_customers(sg_state: str = None):
+    database = DatabaseClient()
     print(f"GET /customers/?{sg_state=}")
     if sg_state:
-        queried_customers = DatabaseClient() \
-                        .query(Customers) \
-                        .filter_by(sg_state=sg_state)
+        queried_customers = database() \
+            .query(Customers) \
+            .filter_by(sg_state=sg_state)
     else:
-        queried_customers = DatabaseClient() \
-                            .query(Customers)
+        queried_customers = database() \
+            .query(Customers)
     return queried_customers
