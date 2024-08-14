9738ca7bdd120b0dfcecea27d0a697a5c6b9786b
Iury Rosal <iuryrosal@gmail.com>
2024-08-14 14:34:17 -0300

    fix: Ajuste na conexão com o database

--- cloudrun-apoenastack-api/apoenastack_api/app.py
+++ cloudrun-apoenastack-api/apoenastack_api/app.py
@@ -5,7 +5,6 @@ from apoenastack_api.database.models import Customers
 from apoenastack_api.database.database_client import DatabaseClient
 
 app = FastAPI()
-database_session = DatabaseClient()
 
 
 @app.get("/health_check", status_code=HTTPStatus.OK, response_model=Message)
@@ -16,7 +15,7 @@ async def health_check():
 @app.get("/customers/{cd_customer}", status_code=HTTPStatus.OK, response_model=Customer)
 async def get_customer_by_cd(cd_customer):
     print(f"GET /customers/{cd_customer}")
-    queried_customer = database_session() \
+    queried_customer = DatabaseClient() \
                         .query(Customers) \
                         .filter_by(cd_customer=cd_customer) \
                         .first()
@@ -27,10 +26,10 @@ async def get_customer_by_cd(cd_customer):
 async def get_customers(sg_state: str = None):
     print(f"GET /customers/?{sg_state=}")
     if sg_state:
-        queried_customers = database_session() \
+        queried_customers = DatabaseClient() \
                         .query(Customers) \
                         .filter_by(sg_state=sg_state)
     else:
-        queried_customers = database_session() \
+        queried_customers = DatabaseClient() \
                             .query(Customers)
     return queried_customers
